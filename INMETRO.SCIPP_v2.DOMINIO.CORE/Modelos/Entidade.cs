790044253912c9376374436241295a28d49d2d74
wtslima <wtslima@gmail.com>
2018-10-25 16:32:49 -0300

    Subindo o Projeto

---
+++ INMETRO.SCIPP_v2.DOMINIO.CORE/Modelos/Entidade.cs
@@ -0,0 +1,7 @@
+﻿
+namespace INMETRO.SCIPP_v2.DOMINIO.CORE.Modelos
+{
+    public class  Entidade
+    {
+    }
+}
---
+++ INMETRO.SCIPP_v2.DOMINIO/Modelos/Organismo.cs
@@ -0,0 +1,19 @@
+﻿using INMETRO.SCIPP_v2.DOMINIO.CORE.Modelos;
+
+namespace INMETRO.SCIPP_v2.DOMINIO.Modelos
+{
+    public class Organismo : Entidade
+    {
+        protected Organismo(){}
+
+        public Organismo(int id, string nome, string codigo)
+        {
+            Id = id;
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public string Nome { get; private set; }
+        public string Codigo { get; private set; }
+    }
+}
